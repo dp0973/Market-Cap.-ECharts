42877461446de149be99481f64b6eda043509854
yeong0973@gmail.com <yeong0973@gmail.com>
2021-03-05 14:15:05 +0900

    style change

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ def change_plain_int(data):
     return data
 
 
-def getcap():
-    raw_data = requests.get('https://finance.naver.com/sise/sise_market_sum.nhn').text
+def getcap(sosok):
+    raw_data = requests.get(f'https://finance.naver.com/sise/sise_market_sum.nhn?sosok={sosok}').text
     soup = BeautifulSoup(raw_data, 'html.parser')
     names = soup.select('tbody > tr > td:nth-child(2) > a', class_='title')
     caps = soup.select('tbody > tr > td:nth-child(7)')
@@ -31,10 +31,19 @@ def getcap():
     return names, caps
 
 @app.route('/')
-@app.route('/home')
-def home():
-    names, caps = getcap()
-    return render_template('home.html', names=names, caps=caps)
+@app.route('/kospi')
+def kospi():
+    names, caps = getcap('0')
+    return render_template('home.html', names=names, caps=caps, sosok='KOSPI')
+
+@app.route('/kosdaq')
+def kosdac():
+    names, caps = getcap('1')
+    return render_template('home.html', names=names, caps=caps, sosok='KOSDAQ')
+    
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('not_found.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
